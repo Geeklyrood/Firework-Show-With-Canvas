c3b865a7606c6602eb844c36235b61ca8c705845
Keenan Rood <Kmrood1@gmail.com>
2017-03-09 11:57:07 -0600

    finished Firework class and started the draw method

--- js/fireworks.js
+++ js/fireworks.js
@@ -58,6 +58,61 @@ function calculateDistance(point1X, point1Y, point2X, point2Y) {
 // Create a Firework particle class constructor
 function Firework(startX, startY, targetX, targetY) {
   
+  this.x = startX;
+  this.y = startY;
+  
+  this.startX = startX;
+  this.startY = startY;
+  
+  this.targetX = targetX;
+  this.targetY = targetY;
+  
+  // distance between starting and ending points
+  this.distanceToTarget = calculateDistance(startX, startY, targetX, targetY);
+  
+  this.distanceTraveled = 0
+  
+  // Track the coordinates of where the Firework particle has been as it 
+  // is flying toward the target to create a trail effect
+  this.coordinates = [];
+  this.coordinateCount = 5;
+  
+  // Populate the initial coordinates collection with the current 
+  // coordinates of the Firework particle.
+  while (this.coordinateCount--) {
+    this.coordinates.push([this.x, this.y]);
+  }
+  
+  this.angle = Math.atan2(targetY - startY, targetX - startX);
+  
+  this.speed = 2;
+  
+  this.acceleration = 1.05;
+  
+  this.brightness = randRange(50, 70);
+  
+  // circle target indicator radius
+  this.targetRadius = 1;
+  
+}
+
+// Draw the Firework particle - method of the Firework 'class'
+Firework.prototype.draw = function() {
+  
+  ctx.beginPath();
+  
+  // Move to the last tracked coordinate (last elemetn) in the
+  // this.cooridinates array and then draw a line to the 
+  // current x and y coordinate.
+  ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], 
+             this.coordinates[this.coordinates.length - 1][1]);
+  
+  ctx.lineTo(this.x, this.y);
+  
+  ctx.strokeStyle = 'hsl(' + hue + ', 100%, ' + this.brightness + '%)';
+  
+  //ctx.stroke();
+  
 }
 
 
